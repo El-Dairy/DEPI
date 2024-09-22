e7b4ab8907c10d5b09d9990513dee1395854a42a
El-Dairy <ahmedeldairy@gmail.com>
2024-09-22 18:30:00 +0300

    khaled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { style } from '@angular/animations';
   // templateUrl: './home/home.component.html',
   styleUrl: './app.component.css',
   template: '<app-home/> <p> Marwa</p> Hello {{city}} {{1+1}} @if (isserverrunning) {<p>Server is running!!! </p>} @else{<p>Not Running !!!</p>} @for(os of operatingsystems;track os.id){<p>{{os.name}}</p>} @for(user of users; track user.id){<p>{{user.name}}</p>} <button type="button" (click)="welcome()" (mouseover)="my_hover()" >Khaled</button>',
-  styles: ['p {color:orange;}'],
+  styles: ['p {<color:red></color:red>;}'],
 })
 export class AppComponent {
   // title = 'khaled_title';
